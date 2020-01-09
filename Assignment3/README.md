19e94d626f2068a2ddb405ab22c3ca2b0b90ce09
Soumya Cherukupalli <soumya27@users.noreply.github.com>
2020-01-09 09:57:45 -0500

    Uploading Assignment 3

---
+++ Assignment3/README.md
@@ -0,0 +1,28 @@
+# Cleaning Website 
+
+Creating a website for a cleaning company using html and styling the page using SCSS.
+
+In this assignment, styled using Grid layout, SCSS features like variables, mixins and inheritance 
+
+## Installation
+1. Clone the repository `git clone git@github.com:neu-mis-info6150-fall-2019/lab-2-cherukupallis.git`.
+2. Navigate to project directory `cd assignment-4-cherukupallis`.
+3. Run `npm install`.
+
+## Build
+Default build mode is `production`. This will build `index.html `, `main.js` and `main.css` files in the `dist` directory.
+1. To build the app, run `npm run build`.
+2. Launch the app by opening the file `./dist/index.html` in a browser.
+
+## Development Server
+Follow below steps to launch the app using `webpack-dev-server`.
+1. Run `npm start` or `npm run start`.
+2. This will open the app on a browser window.
+
+### Author
+* **Soumya Cherukupalli** - *NU001053800*  
+
+## License
+[MIT License](https://opensource.org/licenses/MIT)
+
+
---
+++ Assignment3/webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+let outDirectory = path.resolve(__dirname, 'dist');
+let outputFilename = '[name].js';
+
+module.exports = {
+    mode: 'production',
+    entry: './src/main.js',
+    output: {
+        filename: outputFilename,
+        path: outDirectory,
+        publicPath: '/'
+    },
+    devServer: {
+        contentBase: outDirectory
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            template: './index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        }),
+        new CopyWebpackPlugin([
+            { from:'images',to:'./images'}
+        ])
+    ],
+    module: {
+        rules: [
+            {
+            test: /\.scss$/,
+            use: [{
+                loader: MiniCssExtractPlugin.loader,
+                options: {
+                    publicPath: '../'
+                }
+            }, {
+                loader: "css-loader" // translates CSS into CommonJS
+            }, {
+                loader: "sass-loader" // compiles Sass to CSS
+            }]
+        },
+        {
+            test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
+            loader: 'url-loader?limit=100000'
+        }
+        ]
+    }
+};
